b04fc9cdb35dadb1e796af1adeae07c07b32607e
Warren Galyen <wgalyen@bellhelmets.com>
2021-01-15 12:07:47 -0800

    Clean up error handling and configure reqwests

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,47 @@
 #[macro_use]
 extern crate log;
+extern crate dotenv;
 
+use anyhow::{Context, Result};
 use github_rs::client::{Executor, Github};
 use github_rs::StatusCode;
 use pulldown_cmark::{Event, Parser, Tag};
 use regex::Regex;
+use reqwest::header::{self, HeaderValue};
 use serde_json::Value;
+use std::env;
 use std::fs;
-use std::{env, error::Error};
 use url::Url;
 
 struct Checker {
-    client: Github,
+    reqwest_client: reqwest::blocking::Client,
+    gh_client: Github,
 }
 
 impl Checker {
     /// Creates a new link checker
-    pub fn new(token: String) -> Self {
-        let client = Github::new(token).unwrap();
-        Checker { client }
+    pub fn try_new(token: String) -> Result<Self> {
+        let mut headers = header::HeaderMap::new();
+        // Faking the user agent is necessary for some website, unfortunately.
+        // Otherwise we get a 403 from teh firewall (e.g. Sucuri/Cloudproxy on ldra.com).any
+        headers.insert(header::USER_AGENT, HeaderValue::from_str("curl/7.71.1")?);
+        headers.insert(header::TRANSFER_ENCODING, HeaderValue::from_str("chunked")?);
+
+        let reqwest_client = reqwest::blocking::ClientBuilder::new()
+            .gzip(true)
+            .default_headers(headers)
+            .build()?;
+
+        let gh_client = Github::new(token).unwrap();
+        Ok(Checker {
+            reqwest_client,
+            gh_client,
+        })
     }
 
     fn check_github(&self, owner: String, repo: String) -> bool {
         let (_headers, status, _json) = self
-            .client
+            .gh_client
             .get()
             .repos()
             .owner(&owner)
@@ -34,7 +52,11 @@ impl Checker {
     }
 
     fn check_normal(&self, url: &Url) -> bool {
-        let res = reqwest::blocking::get(url.as_str());
+        let res = self.reqwest_client.get(url.as_str()).send();
+        if res.is_err() {
+            warn!("Cannot send request: {:?}", res);
+            return false;
+        }
         if let Ok(res) = res {
             if res.status().is_success() {
                 true
@@ -48,11 +70,11 @@ impl Checker {
         }
     }
 
-    fn extract_github(&self, url: &str) -> Result<(String, String), Box<dyn Error>> {
+    fn extract_github(&self, url: &str) -> Result<(String, String)> {
         let re = Regex::new(r"github\.com/([^/]*)/([^/]*)")?;
-        let caps = re.captures(&url).ok_or("Invalid capture")?;
-        let owner = caps.get(1).ok_or("Cannot capture owner")?;
-        let repo = caps.get(2).ok_or("Cannot capture repo")?;
+        let caps = re.captures(&url).context("Invalid capture")?;
+        let owner = caps.get(1).context("Cannot capture owner")?;
+        let repo = caps.get(2).context("Cannot capture repo")?;
         Ok((owner.as_str().into(), repo.as_str().into()))
     }
 
@@ -91,7 +113,7 @@ struct Args {
     input: Option<String>,
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<()> {
     pretty_env_logger::init();
 
     let mut args = pico_args::Arguments::from_env();
@@ -100,7 +122,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         input: args.opt_value_from_str(["-i", "--input"])?,
     };
 
-    let checker = Checker::new(env::var("GITHUB_TOKEN")?);
+    let checker = Checker::try_new(env::var("GITHUB_TOKEN")?)?;
     let md = fs::read_to_string(args.input.unwrap_or("README.md".into()))?;
     let links: Vec<Url> = extract_links(&md);
 
@@ -129,7 +151,7 @@ mod test {
 
     #[test]
     fn test_is_github() {
-        let checker = Checker::new("foo".into());
+        let checker = Checker::try_new("foo".into()).unwrap();
         assert_eq!(
             checker
                 .extract_github(&"https://github.com/wgalyen/codesweeper")
@@ -141,7 +163,7 @@ mod test {
     #[test]
     fn test_github() {
         dotenv::dotenv().expect("Failed to read .env file");
-        let checker = Checker::new(env::var("GITHUB_TOKEN").unwrap());
+        let checker = Checker::try_new(env::var("GITHUB_TOKEN").unwrap()).unwrap();
         assert_eq!(
             checker.check(&Url::parse("https://github.com/wgalyen/codesweeper").unwrap()),
             true
@@ -151,7 +173,7 @@ mod test {
     #[test]
     fn test_github_nonexistent() {
         dotenv::dotenv().expect("Failed to read .env file");
-        let checker = Checker::new(env::var("GITHUB_TOKEN").unwrap());
+        let checker = Checker::try_new(env::var("GITHUB_TOKEN").unwrap()).unwrap();
         assert_eq!(
             checker.check(
                 &Url::parse("https://github.com/wgalyen/codesweeper-doesnt-exist-man").unwrap()
@@ -163,7 +185,7 @@ mod test {
     #[test]
     fn test_non_github() {
         dotenv::dotenv().expect("Failed to read .env file");
-        let checker = Checker::new(env::var("GITHUB_TOKEN").unwrap());
+        let checker = Checker::try_new(env::var("GITHUB_TOKEN").unwrap()).unwrap();
         let valid = checker.check(&Url::parse("https://mechanikadesign.com").unwrap());
         assert_eq!(valid, true);
     }
@@ -171,7 +193,7 @@ mod test {
     #[test]
     fn test_non_github_nonexistent() {
         dotenv::dotenv().expect("Failed to read .env file");
-        let checker = Checker::new(env::var("GITHUB_TOKEN").unwrap());
+        let checker = Checker::try_new(env::var("GITHUB_TOKEN").unwrap()).unwrap();
         let valid = checker.check(&Url::parse("https://mechanikadesign.com/abcd").unwrap());
         assert_eq!(valid, false);
     }
