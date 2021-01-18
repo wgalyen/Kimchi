6c0ab0b4860a9c42c9596b37c3b08767982552f8
Warren Galyen <wgalyen@bellhelmets.com>
2021-01-18 09:39:45 -0800

    Add statistics Add support for changing number of threads

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,17 @@ extern crate log;
 use anyhow::Result;
 use regex::RegexSet;
 use std::env;
-use std::fs;
+use std::{collections::HashSet, fs};
 
 mod checker;
 mod extract;
 
-use checker::Checker;
+use checker::{CheckStatus, Checker};
 use extract::extract_links;
 use futures::future::join_all;
 
 use gumdrop::Options;
+use reqwest::Url;
 
 #[derive(Debug, Options)]
 struct KimchiOptions {
@@ -29,6 +30,11 @@ struct KimchiOptions {
     #[options(help = "Maximum number of allowed redirects", default = "10")]
     max_redirects: usize,
 
+    #[options(
+        help = "Number of threads to utilize (defaults to  number of cores available to the system"
+    )]
+    threads: Option<usize>,
+
     #[options(help = "User agent", default = "curl/7.71.1")]
     user_agent: String,
 
@@ -43,12 +49,47 @@ struct KimchiOptions {
     exclude: Vec<String>,
 }
 
-#[tokio::main]
-async fn main() -> Result<()> {
+fn main() -> Result<()> {
     pretty_env_logger::init();
-
     let opts = KimchiOptions::parse_args_default_or_exit();
 
+    let mut runtime = match opts.threads {
+        Some(threads) => {
+            // We define our own runtime instead of the `tokio::main` attribute since we want to make the number of threads configurable
+            tokio::runtime::Builder::new()
+                .threaded_scheduler()
+                .core_threads(threads)
+                .enable_all()
+                .build()?
+        }
+        None => tokio::runtime::Runtime::new()?,
+    };
+    runtime.block_on(run(opts))?;
+    Ok(())
+}
+
+fn print_statistics(found: &HashSet<Url>, results: &Vec<CheckStatus>) {
+    let found = found.len();
+    let excluded: usize = results
+        .iter()
+        .filter(|l| matches!(l, CheckStatus::Excluded))
+        .count();
+    let success: usize = results
+        .iter()
+        .filter(|l| matches!(l, CheckStatus::OK))
+        .count();
+    let errors: usize = found - excluded - success;
+
+    println!("");
+    println!("📝Summary");
+    println!("-------------------");
+    println!("🔍Found: {}", found);
+    println!("👻Excluded: {}", excluded);
+    println!("✅Successful: {}", success);
+    println!("🚫Errors: {}", errors);
+}
+
+async fn run(opts: KimchiOptions) -> Result<()> {
     let excludes = RegexSet::new(opts.exclude).unwrap();
 
     let checker = Checker::try_new(
@@ -63,8 +104,12 @@ async fn main() -> Result<()> {
     let links = extract_links(&md);
 
     let futures: Vec<_> = links.iter().map(|l| checker.check(&l)).collect();
+
     let results = join_all(futures).await;
 
+    if opts.verbose {
+        print_statistics(&links, &results);
+    }
     let errorcode = if results.iter().all(|r| r.is_success()) {
         0
     } else {
